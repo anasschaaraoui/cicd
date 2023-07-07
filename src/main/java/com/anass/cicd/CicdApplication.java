e714b3c2a74983847e18110d9f12efd86ab0d982
Anass <anass.chaaraoui.94@gmail.com>
2023-07-07 14:23:34 +0200

    Dockerfile added

--- src/main/java/com/anass/cicd/CicdApplication.java
+++ src/main/java/com/anass/cicd/CicdApplication.java
@@ -2,10 +2,18 @@ package com.anass.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdApplication {
 
+	@GetMapping("/getMessage")
+	public String getMessage() {
+		return "Hello world 123... !!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
 	}
